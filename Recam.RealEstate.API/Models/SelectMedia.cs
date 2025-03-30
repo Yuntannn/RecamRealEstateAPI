932d3323578af37e0e51537000f5b3c38ddb6c27
Yuntan Sun <yuntan.sun@testpoint.com.au>
2025-03-31 09:35:24 +1100

    selectedMedia

--- Recam.RealEstate.API/Models/SelectMedia.cs
+++ Recam.RealEstate.API/Models/SelectMedia.cs
@@ -3,14 +3,14 @@
     public class SelectMedia
     {
         public int Id { get; set; }
-        public int ListingCaseId { get; set; }
-        public ListingCase ListingCase { get; set; }
-        public string UploadById { get; set; }
+        //public int ListingCaseId { get; set; }
+        //public ListingCase ListingCase { get; set; }
+        public string SelectById { get; set; }
         public User Agent { get; set; }
         public int MediaAssetId { get; set; }
         public MediaAsset MediaAsset { get; set; }
-        public bool? IsHero { get; set; }
-        public bool? DisplayInGallery { get; set; }
+        public bool IsHero { get; set; }
+        public bool DisplayInGallery { get; set; }
         public DateTime SelectAt { get; set; } = DateTime.Now;
 
     }
